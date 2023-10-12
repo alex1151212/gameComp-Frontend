fc163ac920979e052dbaee0a8c0fa0a9382ddf4d
DESKTOP-84MUU1D\alex1151212 <alex1151212@gmail.com>
2023-10-13 05:24:09 +0800

    ✨: pacman animation

---
+++ src/components/pacman/index.tsx
@@ -0,0 +1,8 @@
+export default function Pacman() {
+  return (
+    <div className="pacman">
+      <div className="pacman-top"></div>
+      <div className="pacman-bottom"></div>
+    </div>
+  );
+}
