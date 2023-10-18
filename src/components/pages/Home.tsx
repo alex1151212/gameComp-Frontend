68a682faf773bbe0217008e2ce4b4d157e09a68f
DESKTOP-84MUU1D\alex1151212 <alex1151212@gmail.com>
2023-10-18 15:52:43 +0800

    🚧💄: update home page style

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import Logo from "../../assets/images/logo.png";
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
   return (
     <div className="home">
       <div className="home-logo">
-        <img src={Logo} alt="" />
+        <p>Innovation</p>
+        <div>
+          <span className="home-logo-yello">Game</span>
+          <span>Design</span>
+        </div>
       </div>
       <div className="home-content">
-        <div className="text">創意遊戲設計大賽</div>
-
         <div className="money">
-          <p className="feed">$200,000</p>
+          <p>$200,000</p>
         </div>
         <div className="text">總獎金</div>
       </div>
