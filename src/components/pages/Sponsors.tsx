a29b5716762822f1b15a3cdc788d8a2dd4e1f83f
DESKTOP-84MUU1D\alex1151212 <alex1151212@gmail.com>
2023-10-25 12:44:30 +0800

    🎨💄:  update css layout structure ,some page style

--- src/components/pages/Sponsors.tsx
+++ src/components/pages/Sponsors.tsx
@@ -6,13 +6,16 @@ const Sponsors: React.FC<SponsorsProps> = () => {
   return (
     <div className="sponsors">
       <div className="sponsors-content">
-        <SponsorCard color="#ffff00" />
-        <SponsorCard color="#0000ff" />
-        <SponsorCard color="#ffff00" />
-        <SponsorCard color="#0000ff" />
-        <SponsorCard color="#ffff00" />
-        <SponsorCard color="#0000ff" />
-        <SponsorCard color="#ffff00" />
+        <h1>贊助商</h1>
+        <div className="sponsors-content-card-wrapper">
+          <SponsorCard color="#ffff00" />
+          <SponsorCard color="#0000ff" />
+          <SponsorCard color="#ffff00" />
+          <SponsorCard color="#0000ff" />
+          <SponsorCard color="#ffff00" />
+          <SponsorCard color="#0000ff" />
+          <SponsorCard color="#ffff00" />
+        </div>
       </div>
     </div>
   );
