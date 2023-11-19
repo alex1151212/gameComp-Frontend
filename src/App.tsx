e910a4d82adc25b430e213c6814d7918d281893d
DESKTOP-84MUU1D\alex1151212 <alex1151212@gmail.com>
2023-11-19 22:51:39 +0800

    💄 : update footer ,sponsor-card style

--- src/App.tsx
+++ src/App.tsx
@@ -66,6 +66,14 @@ function App() {
                 </span>
               </div>
             </div>
+            <div className="footer-society">
+              <a style={{ color: "rgb(24,119,242)" }}>
+                <FaFacebookSquare />
+              </a>
+              <a style={{ color: "#833ab4" }}>
+                <FaInstagram />
+              </a>
+            </div>
           </div>
           <div>
             <table>
@@ -76,14 +84,6 @@ function App() {
                 </tr>
               </tbody>
             </table>
-            <div className="footer-society">
-              <a style={{ color: "rgb(24,119,242)" }}>
-                <FaFacebookSquare />
-              </a>
-              <a style={{ color: "#833ab4" }}>
-                <FaInstagram />
-              </a>
-            </div>
           </div>
         </div>
         <div className="footer-border">
