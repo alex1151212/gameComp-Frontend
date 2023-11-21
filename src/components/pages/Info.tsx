a32ee2c98f08b74e34c31bc2ddddddef778d3edd
DESKTOP-84MUU1D\alex1151212 <alex1151212@gmail.com>
2023-11-21 20:12:23 +0800

    📝 : update info pdf file

--- src/components/pages/Info.tsx
+++ src/components/pages/Info.tsx
@@ -30,6 +30,48 @@ const Info: React.FC<InfoProps> = () => {
               </div>
             }
           />
+          <InfoCard
+            title={"創意遊戲設計競賽(贊助企劃書)"}
+            content={
+              <div>
+                <p>112臺灣大專院校創意遊戲設計競賽(贊助企劃書)</p>
+                <a
+                  href="assets/files/112臺灣大專院校創意遊戲設計競賽_贊助企劃書.pdf"
+                  target="_blank"
+                  style={{
+                    fontSize: "42px",
+                  }}
+                >
+                  <IoCloudDownloadSharp
+                    style={{
+                      color: "#00f",
+                    }}
+                  />
+                </a>
+              </div>
+            }
+          />
+          <InfoCard
+            title={"創意遊戲設計競賽(活動企劃書)"}
+            content={
+              <div>
+                <p>112臺灣大專院校創意遊戲設計競賽(活動企劃書)</p>
+                <a
+                  href="assets/files/112臺灣大專院校創意遊戲設計競賽_活動企劃書.pdf"
+                  target="_blank"
+                  style={{
+                    fontSize: "42px",
+                  }}
+                >
+                  <IoCloudDownloadSharp
+                    style={{
+                      color: "#00f",
+                    }}
+                  />
+                </a>
+              </div>
+            }
+          />
         </div>
       </div>
     </div>
