376474a0583a725e3174c313438e35b4a440cc27
DESKTOP-84MUU1D\alex1151212 <alex1151212@gmail.com>
2023-11-25 13:58:58 +0800

    🐛 : build error

--- src/components/pages/Profile/index.tsx
+++ src/components/pages/Profile/index.tsx
@@ -282,7 +282,7 @@ const Profile: React.FC<Props> = () => {
                   />
                   <span>隊伍名稱</span>
                 </div>
-                {values.teamTeacher.map((teacher, index) => (
+                {values.teamTeacher.map((_, index) => (
                   <div className="profile-content-upload-form-add-input">
                     <div className="profile-content-upload-form-row">
                       <div className="profile-content-upload-form-link">
@@ -349,7 +349,7 @@ const Profile: React.FC<Props> = () => {
                     )}
                   </div>
                 ))}
-                {values.teamMember.map((member, index) => {
+                {values.teamMember.map((_, index) => {
                   return (
                     <div className="profile-content-upload-form-add-input">
                       <div className="profile-content-upload-form-link">
