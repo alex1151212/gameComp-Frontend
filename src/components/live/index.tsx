a03b2c11ebed1195bbf996bc6a6383a13c72e521
DESKTOP-84MUU1D\alex1151212 <alex1151212@gmail.com>
2024-01-06 13:31:22 +0800

    ✨: update live url

--- src/components/live/index.tsx
+++ src/components/live/index.tsx
@@ -8,7 +8,7 @@ const LivePage: React.FC<Props> = () => {
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/O93eoaBQiPc?feature=share"
+        src="https://www.youtube.com/embed/xaiF-WFIZI8?feature=share"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
