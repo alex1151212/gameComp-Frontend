f5a7db86e77244741b84ff501f7f48e75f85abcd
DESKTOP-84MUU1D\alex1151212 <alex1151212@gmail.com>
2024-01-08 01:49:40 +0800

    ✨ : add live record to live page

--- src/components/live/index.tsx
+++ src/components/live/index.tsx
@@ -5,15 +5,33 @@ interface Props {}
 const LivePage: React.FC<Props> = () => {
   return (
     <div className="live">
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/xaiF-WFIZI8?feature=share"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+      <div className="live-content">
+        <h1>比賽直播紀錄</h1>
+        <div>第一段</div>
+        <div className="live-content-iframe-wrapper">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/O93eoaBQiPc?feature=share"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div>第二段</div>
+        <div className="live-content-iframe-wrapper">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/xaiF-WFIZI8?feature=share"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 };
